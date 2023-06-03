b0d8bec491b155092e420bc5e2d5bd4473a13409
hirwajeaneric <hirwaminerve25@gmail.com>
2023-06-03 21:52:10 +0200

    Completed all required functionalities.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(calculateTotals());
+  },[cartItems])
+  
   return <main>
     <Navbar />
     <CartContainer />
